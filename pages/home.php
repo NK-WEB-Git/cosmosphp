3a4af4d0fffcb45a06a6c0f434a23b9dda877d28
NK-WEB-Git <nkuinjeustephane@gmail.com>
2015-06-28 16:03:02 +0200

    modifed home

--- pages/home.php
+++ pages/home.php
@@ -1,2 +1,24 @@
+<?php
+
+/* Connexion à une base ODBC avec l'invocation de pilote */
+$dsn = 'mysql:dbname=blog;host=localhost';
+$user = 'root';
+$password = 'root';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $count = $dbh->exec('INSERT INTO article SET titre="Loup de Wall Street", contenu="Grand film avec Leonardo", date="'.date('Y-m-d H:i:s').'"');
+
+    var_dump($count);
+
+} catch (PDOException $e) {
+    echo 'Connexion échouée : ' . $e->getMessage();
+}
+?>
+
+
 <h1>Je suis la homepage</h1>
 <p><a href="index.php?p=single">Aller au single</a></p>
